7e53731a3f75a27feecfeccf17f8f8485d5ee3da
Bryan Afinidad <b.bongato.afinidad@gmail.com>
2024-01-14 02:29:07 -0800

    1/14/24 changes
    hamburger menu

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-function toggleMenu() {
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
-}
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}))
